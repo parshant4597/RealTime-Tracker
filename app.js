a7d65b18cb1a5d6ceda8578e6dc80e1ad4f54d25
Parshant Goyal <parshantgoyal.bt23civ@pec.edu.in>
2025-12-22 16:38:37 +0530

    Fix socket logic and prepare for deployment

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const path = require('path');
 const socketIo = require('socket.io');
 
+const app = express();
 const server = http.createServer(app);
-
 const io = socketIo(server);
-const path = require('path');
 
+// View engine
 app.set('view engine', 'ejs');
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', (socket) => {
-  socket.on('send-location', (data) => {
-    io.emit('receive-location', {
-      id : socket.id,
-      ...data
-    });
-    socket.on('disconnect', () => {
-      io.emit('user-disconnected', socket.id);
-      console.log('A user disconnected');
-    });
-    console.log('Location received:', data);
-    // Here you can handle the received location data as needed
-  }),
-  console.log('A user connected');  
-  });
-
-app.get('/', (req, res) => { 
+// Routes
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Socket.IO
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // Receive location
+  socket.on('send-location', (data) => {
+    io.emit('receive-location', {
+      id: socket.id,
+      ...data
+    });
+
+    console.log('Location received:', data);
+  });
+
+  // Disconnect
+  socket.on('disconnect', () => {
+    io.emit('user-disconnected', socket.id);
+    console.log('A user disconnected:', socket.id);
+  });
+});
+
+// Server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = app;   
+module.exports = app;
